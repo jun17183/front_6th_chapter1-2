43be764219e8dedc4a978e374d60e890a6f71f61
jun17183 <jun17183@naver.com>
2025-07-14 01:12:07 +0900

    feat. createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,21 @@
+function flattenArray(arr) { 
+	const result = []; 
+	
+	for (const item of arr) { 
+		if (Array.isArray(item)) { 
+			result.push(...flattenArray(item)); 
+		} else if (item !== null && item !== undefined && item !== false) {
+      result.push(item); 
+    }
+	} 
+	
+	return result; 
+}
+
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: flattenArray(children),
+  };
 }
